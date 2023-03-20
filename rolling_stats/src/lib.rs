33de7a4becc2fb2bee5757817d20ee0375d29f55
Jacob Gora <>
2023-03-20 17:54:02 -0400

    window_size argument.

--- rolling_stats/src/lib.rs
+++ rolling_stats/src/lib.rs
@@ -1,7 +1,7 @@
 mod ffi;
 mod rolling_stats;
 use polars::prelude::*;
-use pyo3::types::{PyFloat, PyDateTime};
+use pyo3::types::{PyFloat, PyDateTime, PyInt, PyLong};
 
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
@@ -9,8 +9,8 @@ use pyo3::prelude::*;
 use std::collections::HashMap;
 use polars::export::chrono::Timelike;
 
-fn rolling_stats_impl(datetimes: &Series, values: &Series) -> PolarsResult<Series> {
-    let mut binned_rolling_stats = rolling_stats::BinnedRollingStatistics::default();
+fn rolling_stats_impl(datetimes: &Series, values: &Series, window_size: usize) -> PolarsResult<Series> {
+    let mut binned_rolling_stats = rolling_stats::BinnedRollingStatistics::new(window_size);
     Ok(
         datetimes
         .datetime()?
@@ -37,11 +37,12 @@ fn rolling_stats_impl(datetimes: &Series, values: &Series) -> PolarsResult<Serie
 }
 
 #[pyfunction]
-fn pl_rolling_stats(py_datetimes: &PyAny, py_values: &PyAny) -> PyResult<PyObject> {
+fn pl_rolling_stats(py_datetimes: &PyAny, py_values: &PyAny, py_window_size: usize) -> PyResult<PyObject> {
     let series_a = ffi::py_series_to_rust_series(py_datetimes)?;
     let series_b = ffi::py_series_to_rust_series(py_values)?;
 
-    let out = rolling_stats_impl(&series_a, &series_b)
+    let window_size: usize = py_window_size;
+    let out = rolling_stats_impl(&series_a, &series_b, window_size)
         .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
     ffi::rust_series_to_py_series(&out.into_series())
 }
